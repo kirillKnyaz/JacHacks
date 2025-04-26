4b4423969d52061f495f23a01d237f30b8f51e29
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2025-04-26 12:42:44 -0400

    remove gitignore

--- jachacks-backend/src/main/java/com/example/demo/repository/OrganizationRepository.java
+++ jachacks-backend/src/main/java/com/example/demo/repository/OrganizationRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrganizationRepository extends JpaRepository<OrganizationEntity, Long> {4
-    
+public interface OrganizationRepository extends JpaRepository<OrganizationEntity, Long> {
+
     OrganizationEntity findByName(String name);
 }
