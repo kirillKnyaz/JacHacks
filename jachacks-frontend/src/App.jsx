bf17af718bd2cf1bbe2ee35f6235c523f9de0ec1
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2025-04-26 14:30:16 -0400

    ui changes

--- jachacks-frontend/src/App.jsx
+++ jachacks-frontend/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-      <Router>
+    <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
